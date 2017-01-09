99ecf34f9d5c2ab54e9fc60290d8f69ae6b0a969
Matt Robenolt <matt@ydekproductions.com>
2017-01-09 10:49:56 -0800

    webpack: work around broken webpack compression plugin
    See: https://github.com/webpack/compression-webpack-plugin/issues/24

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,9 @@ function getConfig(app) {
   // server as well as nginx when paired with the gzip_static module.
   if (IS_PRODUCTION) {
     config.plugins.push(new (require('compression-webpack-plugin'))({
-      algorithm: 'gzip',
+      algorithm: function(buffer, options, callback) {
+        require('zlib').gzip(buffer, callback);
+      },
       regExp: /\.(js|map|css|svg|html|txt|ico|eot|ttf)$/,
     }));
   }
