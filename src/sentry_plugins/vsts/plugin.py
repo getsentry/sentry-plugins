4f26fc6c16ea4b054c3cde23a1b202f110a4e9a8
David Cramer <dcramer@gmail.com>
2018-03-05 11:22:59 -0800

    ref(vsts): Remove duplicated storage code
    This removes code which is now part of Sentry core.

--- src/sentry_plugins/vsts/plugin.py
+++ src/sentry_plugins/vsts/plugin.py
@@ -4,16 +4,9 @@ easily out of issues detected from Sentry.io """
 
 from __future__ import absolute_import
 
-from django.utils.html import format_html
 from mistune import markdown
-from rest_framework.response import Response
-
-from sentry.api.serializers.models.plugin import PluginSerializer
-from sentry.models import Activity, Event, GroupMeta
-from sentry.signals import issue_tracker_used
-from sentry.utils.http import absolute_uri
-
 from sentry.plugins.bases.issue2 import IssueTrackingPlugin2
+from sentry.utils.http import absolute_uri
 
 from .mixins import VisualStudioMixin
 from .repository_provider import VisualStudioRepositoryProvider
@@ -62,10 +55,6 @@ class VstsPlugin(VisualStudioMixin, IssueTrackingPlugin2):
         return 'Bug {}'.format(issue['id'])
 
     def get_issue_url(self, group, issue, **kwargs):
-        """
-        Given an issue_id (string) return an absolute URL to the issue's
-        details page.
-        """
         return issue['url']
 
     def get_new_issue_fields(self, request, group, event, **kwargs):
@@ -170,185 +159,6 @@ class VstsPlugin(VisualStudioMixin, IssueTrackingPlugin2):
             'url': work_item['_links']['html']['href'],
             'title': work_item['fields']['System.Title'],
         }
-
-    def build_issue(self, group):
-        conf_key = self.get_conf_key()
-        issue = {}
-        for key in self.issue_fields:
-            issue[key] = GroupMeta.objects.get_value(group, '{}:issue_{}'.format(
-                conf_key,
-                key,
-            ), None)
-        if not any(issue.values()):
-            return None
-        return issue
-
-    def has_linked_issue(self, group):
-        return bool(self.build_issue(group))
-
-    def unlink_issue(self, request, group, issue, **kwargs):
-        conf_key = self.get_conf_key()
-        # TODO(dcramer): these shouldn't be hardcoded here
-        for key in self.issue_fields:
-            GroupMeta.objects.unset_value(
-                group, '{}:issue_{}'.format(conf_key, key))
-        return self.redirect(group.get_absolute_url())
-
-    def view_create(self, request, group, **kwargs):
-        auth_errors = self.check_config_and_auth(request, group)
-        if auth_errors:
-            return Response(auth_errors, status=400)
-
-        event = group.get_latest_event()
-        Event.objects.bind_nodes([event], 'data')
-        try:
-            fields = self.get_new_issue_fields(request, group, event, **kwargs)
-        except Exception as e:
-            return self.handle_api_error(e)
-        if request.method == 'GET':
-            return Response(fields)
-
-        errors = self.validate_form(fields, request.DATA)
-        if errors:
-            return Response({'error_type': 'validation', 'errors': errors}, status=400)
-
-        try:
-            issue = self.create_issue(
-                group=group,
-                form_data=request.DATA,
-                request=request,
-            )
-        except Exception as e:
-            return self.handle_api_error(e)
-
-        conf_key = self.get_conf_key()
-        for key in self.issue_fields:
-            meta_name = '{}:issue_{}'.format(conf_key, key)
-            if key in issue:
-                GroupMeta.objects.set_value(group, meta_name, issue[key])
-            else:
-                GroupMeta.objects.unset_value(group, meta_name)
-
-        issue_information = {
-            'title': request.DATA['title'],
-            'provider': self.get_title(),
-            'location': self.get_issue_url(group, issue),
-            'label': self.get_issue_label(group=group, issue=issue),
-        }
-        Activity.objects.create(
-            project=group.project,
-            group=group,
-            type=Activity.CREATE_ISSUE,
-            user=request.user,
-            data=issue_information,
-        )
-
-        issue_tracker_used.send(
-            plugin=self, project=group.project, user=request.user,
-            sender=type(self)
-        )
-        return Response({'issue_url': self.get_issue_url(group=group, issue=issue)})
-
-    def view_link(self, request, group, **kwargs):
-        auth_errors = self.check_config_and_auth(request, group)
-        if auth_errors:
-            return Response(auth_errors, status=400)
-        event = group.get_latest_event()
-        Event.objects.bind_nodes([event], 'data')
-
-        try:
-            fields = self.get_link_existing_issue_fields(
-                request, group, event, **kwargs)
-        except Exception as e:
-            return self.handle_api_error(e)
-        if request.method == 'GET':
-            return Response(fields)
-        errors = self.validate_form(fields, request.DATA)
-        if errors:
-            return Response({'error_type': 'validation', 'errors': errors}, status=400)
-
-        try:
-            issue = self.link_issue(
-                group=group,
-                form_data=request.DATA,
-                request=request,
-            )
-        except Exception as e:
-            return self.handle_api_error(e)
-
-        conf_key = self.get_conf_key()
-        for key in self.issue_fields:
-            meta_name = '{}:issue_{}'.format(conf_key, key)
-            if key in issue:
-                GroupMeta.objects.set_value(group, meta_name, issue[key])
-            else:
-                GroupMeta.objects.unset_value(group, meta_name)
-
-        issue_information = {
-            'title': issue['title'],
-            'provider': self.get_title(),
-            'location': self.get_issue_url(group, issue),
-            'label': self.get_issue_label(group=group, issue=issue),
-        }
-        Activity.objects.create(
-            project=group.project,
-            group=group,
-            type=Activity.CREATE_ISSUE,
-            user=request.user,
-            data=issue_information,
-        )
-        return Response({'message': 'Successfully linked issue.'})
-
-    def view_unlink(self, request, group, **kwargs):
-        auth_errors = self.check_config_and_auth(request, group)
-        if auth_errors:
-            return Response(auth_errors, status=400)
-        issue = self.build_issue(group)
-        if issue and 'unlink' in self.allowed_actions:
-            self.unlink_issue(request, group, issue)
-            return Response({'message': 'Successfully unlinked issue.'})
-        return Response({'message': 'No issues to unlink.'}, status=400)
-
-    def plugin_issues(self, request, group, plugin_issues, **kwargs):
-        if not self.is_configured(request=request, project=group.project):
-            return plugin_issues
-
-        item = {
-            'slug': self.slug,
-            'allowed_actions': self.allowed_actions,
-            'title': self.get_title()
-        }
-        issue = self.build_issue(group)
-        if issue:
-            item['issue'] = {
-                'issue_id': issue.get('id'),
-                'url': self.get_issue_url(group=group, issue=issue),
-                'label': self.get_issue_label(group=group, issue=issue),
-            }
-
-        item.update(PluginSerializer(group.project).serialize(
-            self, None, request.user))
-        plugin_issues.append(item)
-        return plugin_issues
-
-    # TODO: should we get rid of this (move it to react?)
-    def tags(self, request, group, tag_list, **kwargs):
-        if not self.is_configured(request=request, project=group.project):
-            return tag_list
-
-        issue = self.build_issue(group)
-        if not issue:
-            return tag_list
-
-        tag_list.append(
-            format_html(
-                '<a href="{}">{}</a>',
-                self.get_issue_url(group=group, issue=issue),
-                self.get_issue_label(group=group, issue=issue),
-            )
-        )
-
-        return tag_list
 
     def setup(self, bindings):
         bindings.add('repository.provider',
