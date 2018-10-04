239404aa04e4434b6010db38b7d38d557d4acff4
MeredithAnya <meredith.a.heller@gmail.com>
2018-10-04 09:18:04 -0700

    ref(gitlab):Increase member request to 100 results (#404)

--- src/sentry_plugins/gitlab/client.py
+++ src/sentry_plugins/gitlab/client.py
@@ -68,5 +68,5 @@ class GitLabClient(object):
     def list_project_members(self, repo):
         return self.request(
             'GET',
-            '/projects/{}/members'.format(quote(repo, safe='')),
+            '/projects/{}/members?per_page=100'.format(quote(repo, safe='')),
         )
